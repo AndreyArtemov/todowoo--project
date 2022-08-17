718fcaf1843024e349acb8906e9b0eea143cf82d
AndreyArtemov <artwise1966@gmail.com>
2022-08-17 11:16:40 +0300

    Добавил кнопку Save и возможность обновления данных

--- todo/views.py
+++ todo/views.py
@@ -60,4 +60,13 @@ def currenttodos(request):
 
 def viewtodo(request,todo_pk):
     todo = get_object_or_404(Todo,pk=todo_pk)
-    return render(request, 'todo/viewtodo.html',{'todo': todo})
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+    else:
+        try:
+            form = TodoForm(request.POST,instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Bad info'})
